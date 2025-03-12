c8564d02f278152695bc55f328740ad9f2c8eadd
sandeep-0072 <sandeepking0072@gmail.com>
2025-03-12 20:36:31 +0530

    dom create new element

--- dom.js
+++ dom.js
@@ -56,6 +56,33 @@
 // let a=document.querySelector("p");
 // console.log(a.setAttribute("class","new class"))
 
+// let a=document.querySelector("div");
+// console.log(a.style.backgroundColor="green");
+// let a=document.querySelectorAll("body");
+// console.dir(a) 
+// let newbutton=document.createElement("button");
+// newbutton.innerText="click me!";
+// // console.dir(newbutton);
+// let p=document.querySelector("p");
+// p.after(newbutton);
+// let heading=document.createElement("h1");
+// heading.innerHTML="wel come";
+// let a=document.querySelector("body");
+// a.prepend(heading )
 
-let a=document.querySelector("div");
-console.log(a.style.backgroundColor="green");
+// let a=document.createElement("h1");
+// a.innerText="wel come to my project";
+// let i=document.querySelector("body");
+// i.append(a);
+// let a=document.createElement("button");
+// a.innerText="click me!"
+// let i=document.querySelector("main");
+// i.prepend(a);
+// question=1
+// let button=document.createElement("button")
+// button.innerText="CLICK ME!"
+// button.style.color="white";
+// button.style.backgroundColor="red"
+// let i=document.querySelector("body");
+// i.prepend(button);
+let para=document.querySelector("p");
